59e48df5fa78d629355309c814014e49b62dfc38
hwernze <hwernze@gmail.com>
2015-07-23 15:18:53 +0200

    Disconnect socket controller added

--- controllers/disconnect.js
+++ controllers/disconnect.js
@@ -3,22 +3,21 @@ var _ = require('underscore');
 module.exports = function(req) {
     
     function removeUser(userID){
-
+        //Checking through the sessions
         _.each(GLOBAL.sessions, function (session, sessionName) {
+            //Checking through the activeSelections aka userlist
             _.each(session.activeSelections, function (data, id) {
-                if (id == userID) {
                 
+                if (id == userID) {              
                     delete session.activeSelections[userID];
                     //Broadcast to all clients in the room.
                     req.io.room(sessionName).broadcast("selectionsChange", {
                         option:"remove",
                         selectionID: userID
                     });
-
                 }
             })
         });
-
     };
     console.log("DISCONNECT", req.socket.id);
     removeUser(req.socket.id);
